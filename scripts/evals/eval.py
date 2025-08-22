pipecat-ai/pipecat

Add 44 to evals, update evals to support user speaking first

scripts/evals/eval.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: BSD 2-Clause License
 #
 
-import argparse
 import asyncio
 import io
 import os
@@ -87,7 +86,13 @@ class EvalRunner:
     async def assert_eval_false(self):
         await self._queue.put(False)
 
-    async def run_eval(self, example_file: str, prompt: str, eval: Optional[str] = None):
+    async def run_eval(
+        self,
+        example_file: str,
+        prompt: str,
+        eval: Optional[str] = None,
+        user_speaks_first: bool = False,
+    ):
         if not re.match(self._pattern, example_file):
             return
 
@@ -104,7 +109,9 @@ class EvalRunner:
         try:
             tasks = [
                 asyncio.create_task(run_example_pipeline(script_path)),
-                asyncio.create_task(run_eval_pipeline(self, example_file, prompt, eval)),
+                asyncio.create_task(
+                    run_eval_pipeline(self, example_file, prompt, eval, user_speaks_first)
+                ),
             ]
             _, pending = await asyncio.wait(tasks, timeout=EVAL_TIMEOUT_SECS)
             if pending:
@@ -189,7 +196,11 @@ async def run_example_pipeline(script_path: Path):
 
 
 async def run_eval_pipeline(
-    eval_runner: EvalRunner, example_file: str, prompt: str, eval: Optional[str]
+    eval_runner: EvalRunner,
+    example_file: str,
+    prompt: str,
+    eval: Optional[str],
+    user_speaks_first: bool = False,
 ):
     logger.info(f"Starting eval bot")
 
@@ -215,7 +226,7 @@ async def run_eval_pipeline(
 
     tts = CartesiaTTSService(
         api_key=os.getenv("CARTESIA_API_KEY"),
-        voice_id="71a7ad14-091c-4e8e-a314-022ece01c121",  # British Reading Lady
+        voice_id="97f4b8fb-f2fe-444b-bb9a-c109783a857a",  # Nathan
     )
 
     llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"))
@@ -239,15 +250,23 @@ async def run_eval_pipeline(
     )
     tools = ToolsSchema(standard_tools=[eval_function])
 
-    # See if we need to include an eval prompt.
-    eval_prompt = ""
-    if eval:
-        eval_prompt = f"The answer is correct if the user says [{eval}]."
+    if user_speaks_first:
+        # Generic role-playing mode where eval bot speaks first, then evaluates response
+        eval_prompt = (
+            f"After the user responds, evaluate if their response is appropriate for the context and matches: [{eval}]."
+            if eval
+            else "After the user responds, evaluate if their response is appropriate for the context."
+        )
+        system_prompt = f"You will start the conversation by saying: '{prompt}'. {eval_prompt} Then call the eval function with your assessment."
+    else:
+        # Default behavior for Q&A style interactions
+        eval_prompt = f"The answer is correct if the user says [{eval}]." if eval else ""
+        system_prompt = f"You are an LLM eval, be extremely brief. Your goal is to only ask one question: {prompt}. Call the eval function only if the user answers the question and check if the answer is correct (words as numbers are valid). {eval_prompt}"
 
     messages = [
         {
             "role": "system",
-            "content": f"You are an LLM eval, be extremly brief. Your goal is to only ask one question: {prompt}. Call the eval function only if the user answers the question and check if the answer is correct (words as numbers are valid). {eval_prompt}",
+            "content": system_prompt,
         },
     ]
 
@@ -286,6 +305,14 @@ async def run_eval_pipeline(
     async def on_client_connected(transport, client):
         logger.info(f"Client connected")
         await audio_buffer.start_recording()
+
+        # Default behavior is for the bot to speak first
+        # If the eval bot speaks first, we append the prompt to the messages
+        if user_speaks_first:
+            messages.append(
+                {"role": "user", "content": f"Start by saying this exactly: '{prompt}'"}
+            )
+            await task.queue_frames([context_aggregator.user().get_context_frame()])
 
     @transport.event_handler("on_client_disconnected")
     async def on_client_disconnected(transport, client):

scripts/evals/run-release-evals.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from eval import EvalRunner
 from loguru import logger
+from test import test_tuple
 from utils import check_env_variables
 
 load_dotenv(override=True)
@@ -21,6 +22,8 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 
 FOUNDATIONAL_DIR = SCRIPT_DIR.parent.parent / "examples" / "foundational"
 
+# User speaks first
+USER_SPEAKS_FIRST = True
 
 # Math
 PROMPT_SIMPLE_MATH = "A simple math addition."
@@ -34,6 +37,12 @@ EVAL_WEATHER = (
 # Online search
 PROMPT_ONLINE_SEARCH = "What's the date right now in London?"
 EVAL_ONLINE_SEARCH = f"Today is {datetime.now(timezone.utc).strftime('%B %d, %Y')}."
+
+# Voicemail
+PROMPT_VOICEMAIL = "Please leave a message after the beep."
+EVAL_VOICEMAIL = "Assess the conversation and determine if it is a voicemail."
+PROMPT_CONVERSATION = "Hello, this is Mark."
+EVAL_CONVERSATION = "A start of a conversation, not a voicemail."
 
 TESTS_07 = [
     # 07 series
@@ -132,6 +141,11 @@ TESTS_43 = [
     ("43a-heygen-video-service.py", PROMPT_SIMPLE_MATH, None),
 ]
 
+TESTS_44 = [
+    ("44-voicemail-detection.py", PROMPT_VOICEMAIL, EVAL_VOICEMAIL, USER_SPEAKS_FIRST),
+    ("44-voicemail-detection.py", PROMPT_CONVERSATION, EVAL_CONVERSATION, USER_SPEAKS_FIRST),
+]
+
 TESTS = [
     *TESTS_07,
     *TESTS_14,
@@ -141,6 +155,7 @@ TESTS = [
     *TESTS_27,
     *TESTS_40,
     *TESTS_43,
+    *TESTS_44,
 ]
 
 
@@ -162,8 +177,14 @@ async def main(args: argparse.Namespace):
         log_level=log_level,
     )
 
-    for test, prompt, eval in TESTS:
-        await runner.run_eval(test, prompt, eval)
+    for test_tuple in TESTS:
+        if len(test_tuple) == 3:
+            test, prompt, eval = test_tuple
+            user_speaks_first = False
+        else:
+            test, prompt, eval, user_speaks_first = test_tuple
+
+        await runner.run_eval(test, prompt, eval, user_speaks_first)
 
     runner.print_results()
 

Levidev15/Botelier2.0

scripts/evals/eval.py
@@ -89,7 +89,13 @@ class EvalRunner:
     async def assert_eval_false(self):
         await self._queue.put(False)
 
-    async def run_eval(self, example_file: str, prompt: EvalPrompt, eval: Optional[str] = None):
+    async def run_eval(
+        self,
+        example_file: str,
+        prompt: EvalPrompt,
+        eval: Optional[str] = None,
+        user_speaks_first: bool = False,
+    ):
         if not re.match(self._pattern, example_file):
             return
 
@@ -106,7 +112,9 @@ class EvalRunner:
         try:
             tasks = [
                 asyncio.create_task(run_example_pipeline(script_path)),
-                asyncio.create_task(run_eval_pipeline(self, example_file, prompt, eval)),
+                asyncio.create_task(
+                    run_eval_pipeline(self, example_file, prompt, eval, user_speaks_first)
+                ),
             ]
             _, pending = await asyncio.wait(tasks, timeout=EVAL_TIMEOUT_SECS)
             if pending:
@@ -196,6 +204,7 @@ async def run_eval_pipeline(
     example_file: str,
     prompt: EvalPrompt,
     eval: Optional[str],
+    user_speaks_first: bool = False,
 ):
     logger.info(f"Starting eval bot")
 
@@ -225,7 +234,7 @@ async def run_eval_pipeline(
 
     tts = CartesiaTTSService(
         api_key=os.getenv("CARTESIA_API_KEY"),
-        voice_id="71a7ad14-091c-4e8e-a314-022ece01c121",  # British Reading Lady
+        voice_id="97f4b8fb-f2fe-444b-bb9a-c109783a857a",  # Nathan
     )
 
     llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"))
@@ -260,12 +269,17 @@ async def run_eval_pipeline(
     # See if we need to include an eval prompt.
     eval_prompt = ""
     if eval:
-        eval_prompt = f"The answer is correct if the user says [{eval}]."
+        if user_speaks_first:
+            eval_prompt = f"After the user responds, evaluate if their response is appropriate for the context and matches: [{eval}]."
+            system_prompt = f"You will start the conversation by saying: '{prompt}'. {eval_prompt} Then call the eval function with your assessment."
+        else:
+            eval_prompt = f"The answer is correct if the user says [{eval}]."
+            system_prompt = f"You are an LLM eval, be extremly brief. Your goal is to only ask one question: {example_prompt}. Call the eval function only if the user answers the question and check if the answer is correct (words as numbers are valid). {eval_prompt}"
 
     messages = [
         {
             "role": "system",
-            "content": f"You are an LLM eval, be extremly brief. Your goal is to only ask one question: {example_prompt}. Call the eval function only if the user answers the question and check if the answer is correct (words as numbers are valid). {eval_prompt}",
+            "content": system_prompt,
         },
     ]
 
@@ -312,6 +326,14 @@ async def run_eval_pipeline(
                 )
             )
         await audio_buffer.start_recording()
+
+        # Default behavior is for the bot to speak first
+        # If the eval bot speaks first, we append the prompt to the messages
+        if user_speaks_first:
+            messages.append(
+                {"role": "user", "content": f"Start by saying this exactly: '{prompt}'"}
+            )
+            await task.queue_frames([context_aggregator.user().get_context_frame()])
 
     @transport.event_handler("on_client_disconnected")
     async def on_client_disconnected(transport, client):

scripts/evals/run-release-evals.py
@@ -24,6 +24,8 @@ ASSETS_DIR = SCRIPT_DIR / "assets"
 
 FOUNDATIONAL_DIR = SCRIPT_DIR.parent.parent / "examples" / "foundational"
 
+# User speaks first
+USER_SPEAKS_FIRST = True
 
 # Math
 PROMPT_SIMPLE_MATH = "A simple math addition."
@@ -45,6 +47,12 @@ EVAL_SWITCH_LANGUAGE = "Check if the user is now talking in Spanish."
 # Vision
 PROMPT_VISION = ("What do you see?", Image.open(ASSETS_DIR / "cat.jpg"))
 EVAL_VISION = "A cat description."
+
+# Voicemail
+PROMPT_VOICEMAIL = "Please leave a message after the beep."
+EVAL_VOICEMAIL = "Assess the conversation and determine if it is a voicemail."
+PROMPT_CONVERSATION = "Hello, this is Mark."
+EVAL_CONVERSATION = "A start of a conversation, not a voicemail."
 
 TESTS_07 = [
     # 07 series
@@ -157,6 +165,11 @@ TESTS_43 = [
     ("43a-heygen-video-service.py", PROMPT_SIMPLE_MATH, None),
 ]
 
+TESTS_44 = [
+    ("44-voicemail-detection.py", PROMPT_VOICEMAIL, EVAL_VOICEMAIL, USER_SPEAKS_FIRST),
+    ("44-voicemail-detection.py", PROMPT_CONVERSATION, EVAL_CONVERSATION, USER_SPEAKS_FIRST),
+]
+
 TESTS = [
     *TESTS_07,
     *TESTS_12,
@@ -168,6 +181,7 @@ TESTS = [
     *TESTS_27,
     *TESTS_40,
     *TESTS_43,
+    *TESTS_44,
 ]
 
 
@@ -189,8 +203,15 @@ async def main(args: argparse.Namespace):
         log_level=log_level,
     )
 
-    for test, prompt, eval in TESTS:
-        await runner.run_eval(test, prompt, eval)
+    # Parse test config: (test, prompt, eval) or (test, prompt, eval, user_speaks_first)
+    for test_config in TESTS:
+        if len(test_config) == 3:
+            test, prompt, eval = test_config
+            user_speaks_first = False
+        else:
+            test, prompt, eval, user_speaks_first = test_config
+
+        await runner.run_eval(test, prompt, eval, user_speaks_first)
 
     runner.print_results()
 